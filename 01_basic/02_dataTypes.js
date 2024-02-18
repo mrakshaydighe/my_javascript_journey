418a2b5d8dcf0134350e1c00ad25baa542852629
mrakshaydighe <125887016+mrakshaydighe@users.noreply.github.com>
2024-02-18 21:52:06 +0530

    Basic things added
    Today we added Variables types,datatypes in js ,conversion and more basic things

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,3 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +   // dont write code like that
+     3) // code readability should be high 
+
+console.log("Akshay")
+
+
+let name = "Akshay"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
 // undefined and null
 
 var string;
@@ -41,6 +71,5 @@ console.log(typeof(num4));
 // spelling is in camel case B and I is in uppercase
 // another process for big int is add n after no.
 // we cannot did operation with bigint with other data types
-
 
 
